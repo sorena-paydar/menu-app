6f8d168abfb6e79e06c988f2b9a788f7136fe471
Sorena <sorenapaydar81@gmail.com>
2024-06-20 01:59:06 +0330

    feat: useApi hook

---
+++ src/hooks/useAPI.ts
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { UseApiRequestOptions, UseApiRequestResponse } from '@/types/api';
+
+const useApiRequest = <T>({
+  route,
+  method,
+  initialData,
+}: UseApiRequestOptions): UseApiRequestResponse<T> => {
+  const [data, setData] = useState<T | null>(initialData ?? null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.request<T>({
+        url: route,
+        method,
+      });
+
+      setData(response.data);
+    } catch (err) {
+      // @ts-ignore
+      setError(err.message || 'An error occurred');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    data,
+    loading,
+    error,
+    fetchData,
+  };
+};
+
+export default useApiRequest;
---
+++ src/types/api.d.ts
@@ -0,0 +1,14 @@
+import {Method} from "axios";
+
+export interface UseApiRequestResponse<T> {
+    data: T | null;
+    loading: boolean;
+    error: string | null;
+    fetchData: () => Promise<void>;
+}
+
+export interface UseApiRequestOptions {
+    route: string;
+    method: Method;
+    initialData?: any;
+}
